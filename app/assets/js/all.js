2dd36163cfcbb3d2a9eb25536d42e6ebef0d59d7
ChienChih <ana31005@gmail.com>
2021-06-21 09:30:43 +0800

    update

---
+++ app/assets/js/all.js
@@ -0,0 +1,7 @@
+var swiper = new Swiper(".mySwiper", {});
+var swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 20,
+    slidesPerView: 1,
+    watchSlidesVisibility: true,
+    loop: true,
+});
