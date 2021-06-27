c4822cf68e00793fe7cde423229bce3839996b9e
Eric <ana31005@gmail.com>
2021-06-28 01:13:55 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,7 +1,23 @@
 var swiper = new Swiper(".mySwiper", {});
-var swiper2 = new Swiper(".mySwiper2", {
+var swiper_left = new Swiper(".mySwiper-left", {
     spaceBetween: 20,
-    slidesPerView: 1,
-    watchSlidesVisibility: true,
+    slidesPerView: "auto",
     loop: true,
+});
+var swiper_right = new Swiper(".mySwiper-right", {
+    spaceBetween: 20,
+    slidesPerView: "auto",
+    loop: true,
+});
+var swiper_right = new Swiper(".mySwiper-video", {
+    spaceBetween: 20,
+    slidesPerView: "auto",
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    pagination: {
+        el: ".swiper-pagination",
+    },
 });
