8b640bcbe6037f52d8323bf9f249eb31cc7603a5
Windows-Yen <ana31005@gmail.com>
2021-07-07 13:37:37 +0800

    課程頁面更新

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,5 @@
-var swiper = new Swiper(".mySwiper", {
-    
+// swiper 設定
+var swiper = new Swiper(".mySwiper", {    
 });
 var swiper_left = new Swiper(".mySwiper-left", {
     spaceBetween: 20,
@@ -24,8 +24,10 @@ var swiper_right = new Swiper(".mySwiper-video", {
     },
 });
 
+// 載入 wow.js
 new WOW().init();
 
+// 右邊 sidebar 切換
 $('.dropdown-toggle').click(function(e){
     e.preventDefault();
     $('.header .fixed-right .icon').slideToggle()
@@ -40,3 +42,22 @@ $('.fixed-black .menu').mouseleave(function(){
     $('.fixed-black-box').show();
     $('.fixed-black .menu').hide();
 })
+
+$('.fixed-black .menu').hide();
+
+// 分頁功能
+// Show the first tab and hide the rest
+$('.tabs-nav li:first-child').addClass('active');
+$('.tab-content').hide();
+$('.tab-content:first').show();
+
+// Click function
+$('.tabs-nav li').click(function(){
+  $('.tabs-nav li').removeClass('active');
+  $(this).addClass('active');
+  $('.tab-content').hide();
+  
+  var activeTab = $(this).find('a').attr('href');
+  $(activeTab).fadeIn();
+  return false;
+});
